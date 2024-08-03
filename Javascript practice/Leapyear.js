1f207c8ff6f0b94e662f71d3fd717350b7a0c73d
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-03 17:01:38 +0600

    return without string pre dec

--- Javascript practice/Leapyear.js
+++ Javascript practice/Leapyear.js
@@ -2,10 +2,9 @@ var year = prompt("Input year");
 alert("The year "+ year + Leap(year));
 
 function Leap(year){
-    var m;
     if(((year%4==0) && (year%100 !=0)) || (year%400==0)){
-        m = " is a leap year";
+        return " is a leap year";
     }else{
-        m = " is not a leap year.";
-    }return m;
+        return " is not a leap year.";
+    }
 }
