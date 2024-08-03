7a4447c2003948fce77a90ac4b2308de99610401
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-03 17:29:23 +0600

    guest return condition intruption

---
+++ Javascript practice/guestCheck.js.js
@@ -0,0 +1,12 @@
+var name = prompt("Enter your name");
+alert(name + check(name));
+
+function check(name){
+var guests = ["Navin","Eva","Amrin","Jack","Anne"];
+    
+for (var i = 0; i<guests.length; i++){
+    if(name == guests[i]){
+        return ", welcome to the party!";
+    }
+}return ", go to hell!";
+}
