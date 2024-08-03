6a2bc126490d9cceacac362197c79c7a2b3317c2
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-03 17:36:36 +0600

    ternary not supported & interpolation not supported

--- Javascript practice/guestCheck.js.js
+++ Javascript practice/guestCheck.js.js
@@ -2,11 +2,11 @@ var name = prompt("Enter your name");
 alert(name + check(name));
 
 function check(name){
-var guests = ["Navin","Eva","Amrin","Jack","Anne"];
+    var guests = ["Navin","Eva","Amrin","Jack","Anne"];
     
-for (var i = 0; i<guests.length; i++){
-    if(name == guests[i]){
-        return ", welcome to the party!";
-    }
-}return ", go to hell!";
+    for (var i = 0; i<guests.length; i++){
+        if(name == guests[i]){
+            return ", welcome to the party!";
+        }
+    }return ", puta madre!";
 }
