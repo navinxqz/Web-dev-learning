d575bc90dc3f09dc94b6fc23c05c02635ced2e09
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-04 02:27:01 +0600

    value with num

--- Javascript practice/whileSong.js
+++ Javascript practice/whileSong.js
@@ -1,21 +1,15 @@
 function sing(){
     var n = 9;
     var noun = "bottles";
+    var value = n;
 
-    while(n>0){
-        // if(n==0){
-        //     console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
-        //     n--;
-        // }else{
-        //     console.log(n +" bottles of beer on the wall, "+ n+" bottles of beer. Take one down and pass it around, "+(n-1)+" bottles of beer on the wall.");
-        //     n--;
-        // }
+    while(n>=0){
         if(n==1){ noun = "bottle"; }
-        console.log(n+" "+noun+" of beer on the wall, "+n+" "+noun+" of beer.");
+        if(n==0){ value = "No more"; }
+        console.log(value+" "+noun+" of beer on the wall, "+value+" "+noun+" of beer.");
         n--;
 
-        var value = n;
-        if(n==0){ value = "No more"; }
+        value = n;
         console.log("Take one down and pass it around, "+value+" "+noun+" of beer on the wall.");
     }
 }
