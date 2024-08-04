9d4b48feb5cc362b591820ab2e01787f664dd482
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-05 01:12:05 +0600

    js func all with a for loop

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,6 @@
-function click(){
-    alert("cliked me!");
-}
 
-document.querySelector("button").addEventListener("click", click);
+for(var i=0; i<document.querySelectorAll(".drum").length; i++){
+document.querySelectorAll(".set button")[i].addEventListener("click", function(){
+    alert("cliked me!");
+});
+}
