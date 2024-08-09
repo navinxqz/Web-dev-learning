4a0976963c17edb5dc903c0c50b63431cb635147
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-09 23:10:43 +0600

    jQ eventlistener

--- jQuery/index.js
+++ jQuery/index.js
@@ -5,8 +5,12 @@ $("h1").addClass("big margin50");
 
 $("a").attr("href","https://www.yahoo.com");
 
-for(var i=0;i<5;i++){
-    document.querySelectorAll("button")[i].addEventListener("click",function(){
-        document.querySelector("h1").style.color = "purple";
-    });
-}
+// for(var i=0;i<5;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }
+
+$("button").click(function(){
+    $("h1").css("color","pink");
+});
