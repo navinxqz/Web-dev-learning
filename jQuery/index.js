88bbfd83f7e6e7b7ad214b2c70478d8961eec1c9
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-09 23:07:46 +0600

    js color cng with event listener

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,6 +1,12 @@
 $("h1").addClass("big margin50");
-$("h1").click(function(){
-    $("h1").css("color","purple");
-})
+// $("h1").click(function(){
+//     $("h1").css("color","purple");
+// });
 
 $("a").attr("href","https://www.yahoo.com");
+
+for(var i=0;i<5;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        document.querySelector("h1").style.color = "purple";
+    });
+}
