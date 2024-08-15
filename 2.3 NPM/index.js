a93c33767f925934d24fee4b799818eb263c6bc8
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-16 00:30:37 +0600

    new esm added with old one

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,5 +1,8 @@
 import generateName from "sillyname";
+import SuperHero, { randomSuperhero } from "superheroes";
 // var generateName = require("sillyname");
 var sillyName = generateName();
+const superHero = randomSuperhero();
 
-console.log(`My name is ${sillyName}.`);
+console.log(`My name is ${sillyName}. And I am the ${superHero}!`);
+
