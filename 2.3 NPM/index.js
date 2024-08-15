b2bcda70204e99e1e7b0bf8c5a1deec461f7eee1
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-16 00:21:32 +0600

    with ESM

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,4 +1,5 @@
-var generateName = require("sillyname");
+import generateName from "sillyname";
+// var generateName = require("sillyname");
 var sillyName = generateName();
 
 console.log(`My name is ${sillyName}.`);
