53368100269094d2e58bba9fb77b1e44eb0d9f5d
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-17 09:26:36 +0600

    url link saved as txt

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -1,7 +1,3 @@
-/* 
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
 import inquirer from 'inquirer';
 import qr from 'qr-image';
 import fs from 'fs';
@@ -15,6 +11,11 @@ inquirer
  
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
   })
   .catch((error) => {
     if (error.isTtyError) {
