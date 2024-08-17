21b3340e66de1d9af28a3028ac3ee482b3f5b27e
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-17 08:59:13 +0600

    inquirer & qr-image package added

---
+++ 2.4 QR Code Project/index.js
@@ -0,0 +1,5 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
