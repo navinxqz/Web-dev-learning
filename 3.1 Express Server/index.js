985702f410b3150a41f0d9fabf0ef809ec881ff6
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-18 20:38:13 +0600

    sending res for the req

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -2,10 +2,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) =>{
+  res.send("Hello World!");
+})
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+  console.log(`Server runnin on port ${port}.`);
 });
-    Requests
+
+    // Requests
 // Get = Request resources
 // post = Sending resourses
 // put & patch = update method. put -> replace a res, patch -> patch up a res
