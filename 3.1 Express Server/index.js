6dfecc08e9de118a53c2c7be482a985049243ec8
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-18 20:33:59 +0600

    kind of http req

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -5,3 +5,8 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
+    Requests
+// Get = Request resources
+// post = Sending resourses
+// put & patch = update method. put -> replace a res, patch -> patch up a res
+// Delete = delete res
