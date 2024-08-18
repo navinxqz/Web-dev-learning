b07dca7556c20864fa28555aa855d3727e6327d3
navin-nawshin <navinmdnawshin@gmail.com>
2024-08-18 23:41:55 +0600

    multipage req

--- 3.2 HTTP req/index.js
+++ 3.2 HTTP req/index.js
@@ -1,6 +1,19 @@
 import express from "express";
 var app = express();
 
+app.get("/", (req,res) =>{
+    // console.log(req.rawHeaders);
+    res.send("<h1>Hello!</h1>");
+});
+
+app.get("/about", (req,res) =>{
+    res.send("<h1>About ME</h1><p>My name is Navin and I am a human.</p>");
+});
+
+app.get("/contact", (req,res) =>{
+    res.send("<h1>Contact</h1><p>To connect with me: cyt.navin6@gmail.com</p>");
+});
+
 app.listen(3000,() =>{
     console.log("Server 3000 running...");
-})
+});
